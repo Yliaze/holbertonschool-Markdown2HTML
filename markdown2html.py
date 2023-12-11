28bb77a9e99e28adf2a8134dc87cc2309f038528
Benjamin <benjamin.alazet@gmail.com>
2023-12-11 10:28:28 +0100

    Documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that takes an argument 2 strings:
+"""Script that takes an argument 2 strings:
 
 First argument is the name of the Markdown file
-Second argument is the output file name
-"""
+Second argument is the output file name"""
 import sys
 import os
 
-if len(sys.argv) < 3:
-    """Error if (sys.argv) < 3"""
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        """Error if (sys.argv) < 3"""
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
 
-if not os.path.exists(sys.argv[1]):
-    """Error sys.argv[1] doesn't exist"""
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    exit(1)
+    if not os.path.exists(sys.argv[1]):
+        """Error sys.argv[1] doesn't exist"""
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        exit(1)
 
