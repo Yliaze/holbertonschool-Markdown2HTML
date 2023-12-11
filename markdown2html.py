8212c981bc535ae26e78d1d98c45e2e97d466b00
Benjamin <benjamin.alazet@gmail.com>
2023-12-11 10:25:21 +0100

    Add documentation

--- markdown2html.py
+++ markdown2html.py
@@ -9,9 +9,12 @@ import sys
 import os
 
 if len(sys.argv) < 3:
+    """Error if (sys.argv) < 3"""
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     exit(1)
 
 if not os.path.exists(sys.argv[1]):
+    """Error sys.argv[1] doesn't exist"""
     print(f"Missing {sys.argv[1]}", file=sys.stderr)
     exit(1)
+
