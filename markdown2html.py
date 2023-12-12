b9fce60591c39c70e8a9c8e9d406f10737205113
Benjamin <benjamin.alazet@gmail.com>
2023-12-12 10:38:04 +0100

    Unordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -1,40 +1,46 @@
 #!/usr/bin/python3
-"""Markdown2 rebuilt"""
+"""Script that takes an argument 2 strings:
 
+First argument is the name of the Markdown file
+Second argument is the output file name"""
 
 import sys
 import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        # Error if (sys.argv) < 3
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
-
+        
     if not os.path.exists(sys.argv[1]):
-        # Error sys.argv[1] doesn't exist
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         exit(1)
-    
-    markdown_file = sys.argv[1]
-    html_file = sys.argv[2]
-
-    # Open the markdown file
-    with open(markdown_file, 'r', encoding='UTF-8') as file:
-        all_lines = file.readlines()
-
-        output_lines = []
         
-        for lines in all_lines:
-            if lines.startswith('#'):
-                # Count number of #
-                count = lines.count('#')
-                # Suppr space and # from text
-                text = lines[count:].strip()
-                # Format text
-                html_title = f'<h{count}>{text}</h{count}>\n'
-                output_lines.append(html_title)
+    readme = sys.argv[1]
+    readhtml = sys.argv[2]
     
-    # write in the html file
-    with open(html_file, 'w') as output_file:
-        output_file.writelines(output_lines)
+    with open(readme, 'r', encoding='utf-8') as readme_file:
+        lines = readme_file.readlines()
+        
+        with open(readhtml, 'w', encoding='utf-8') as tohtml:
+            in_list = False
+            
+            for line in lines:
+                if line.startswith('-'):
+                    if not in_list:
+                        tohtml.write("<ul>\n")
+                        in_list = True
+                    tohtml.write(f"<li>{line.lstrip('-').strip()}</li>\n")
+                else:
+                    if in_list:
+                        tohtml.write("</ul>\n")
+                        in_list = False
+                    count = 0
+                    for char in line:
+                        if char == '#':
+                            count += 1
+                    if count > 0:
+                        tohtml.write(f"<h{count}>{line.strip('#').strip()}</h{count}>\n")
+
+            if in_list:
+                tohtml.write("</ul>\n")
